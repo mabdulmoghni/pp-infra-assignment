4c0c5d1d48f2f924828a3bcc510054b0edc4f53b
mabdulmoghni <eng.mabdulmoghni@gmail.com>
2024-03-19 06:08:45 +0200

    update ci terraform version

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "eks" {
 
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnets
-  
+
 }
 
 ################################################################################
